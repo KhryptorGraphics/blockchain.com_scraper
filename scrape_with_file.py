b1dfd3f72f1761d6b4ba700ccf8b3aa2c635f96b
Shibu Thomas <51987227+shibu130@users.noreply.github.com>
2021-05-08 14:41:58 +0530

    little changes

--- scrape_with_file.py
+++ scrape_with_file.py
@@ -5,30 +5,32 @@ from time import sleep
 
 def main(address):
 
-    t1=perf_counter()
+    try:
+        t1=perf_counter()
 
-    url="https://www.blockchain.com/btc/address/"+address
-    page=req.get(url)
+        url="https://www.blockchain.com/btc/address/"+address
+        page=req.get(url)
 
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+        soup = BeautifulSoup(page.content, 'html.parser')
 
-    a=soup.find_all('span',class_="sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC")  # span had changed 
+        a=soup.find_all('span',class_="sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC")
 
-    count=0
-    for i in a:
-        for j in i:
-            count+=1
-            if(count==6):
+        count=0
+        for i in a:
+            for j in i:
+                count+=1
+                if(count==6):
                 # print(i.contents[0].split(" ")[0])
+                    print("balance is "+i.contents[0].split(" ")[0]+ " for "+address)
+                    if float(i.contents[0].split(" ")[0]):  # comment line 26 and 27 if u dont want to stop if balance is greater than 0 for an address
+                        input("continue ??")
+                    t2=perf_counter()
 
-                print("balance is "+i.contents[0].split(" ")[0]+ " for "+address)
-                if float(i.contents[0].split(" ")[0]):  #comment lines 26 and 27 if u dont want to stop if the balance of that particular address is greater than 0
-                    input("continue ??") 
-    t2=perf_counter()
-
-    print("{0} time elaspsed".format(str(t2-t1)))
-    # print(a)
+        print("{0} time elaspsed".format(str(t2-t1)))
+    except KeyboardInterrupt:
+        print("crtl + c pressed exiting")   #crtl + c stops script
+        exit()
 
 
 with open("address.txt","r+") as r:
